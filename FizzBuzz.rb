334b3d3f952831be7d32d6e1df4f9a38195bcb90
Cameron <cameronjamesbishop@googlemail.com>
2023-04-04 11:39:10 +0000

    FizzBuzz Complete

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -3,15 +3,15 @@ print ('enter a number: ')
 number = gets.chomp.to_i()
 
 def num_change(number)
-	if  number % 3 == 0 && number % 5 == 0
-		puts "fizzbuzz"
+	if number % 3 == 0 && number % 5 == 0
+		return "fizzbuzz"
 	elsif number % 5 == 0
-		puts "buzz"
+		return "buzz"
 	elsif number % 3 == 0
-		puts "fizz"
+		return "fizz"
 	else
 		puts number
 	end
 end
 
-num_change(number)
+puts num_change(number)
